a6adfcd0ffb3b92ef00a7315e90ba2f4e9ce88a2
MegaDraconius <ivanvclinetor@gmail.com>
2024-04-15 19:43:45 +0200

    Solve exercise 2 ,3 and 4

--- src/songs.js
+++ src/songs.js
@@ -2,6 +2,7 @@
 
 function getAllArtists(array) {
   let result = array.map((array) => array.artist);
+  console.log(result);
 
   //console.log("Exercise 1 ->", result);
   return result;
@@ -9,16 +10,27 @@ function getAllArtists(array) {
 
 //Exercise 2: Get the songs of a certain artist
 function getSongsFromArtist(array, artist) {
+  return array.filter((array) => array.artist === artist);
   //Write your code here
 }
 
 //Exercise 3: Alphabetic order by title
-function orderAlphabetically() {
+function orderAlphabetically(array) {
+  const sortedTitles = array
+    .map((arrayOrderedAlphabetically) => arrayOrderedAlphabetically.title)
+    .sort();
+
+  return sortedTitles.slice(0, 10);
   //Write your code here
 }
 
 //Exercise 4: Order by year, ascending
 function orderByYear() {
+  const sortedYear = array
+    .map((arrayOrderByYear) => arrayOrderByYear.year)
+    .sort();
+
+  return sortedYear.slice(0, 10);
   //Write your code here
 }
 
